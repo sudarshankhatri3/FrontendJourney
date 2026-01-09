bf389dba9abbe97781ec704dc0755ac431b1b024
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-01-09 23:29:22 +0545

    almost complete portfolio

--- learnDsaSeries/python/singlyLinked.py
+++ learnDsaSeries/python/singlyLinked.py
@@ -1,8 +1,11 @@
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
 
 
 
-        
+x="python"
+y=x
+x=x.upper()
+print(y)
