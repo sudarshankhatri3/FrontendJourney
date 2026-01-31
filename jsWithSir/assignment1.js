83176ec19d5821b1cf31625bb02023f53ae6f987
sudarshankhatri3 <khatrisudarshan360@gmail.com>
2026-01-31 15:00:02 +0545

    mybank

--- jsWithSir/assignment1.js
+++ jsWithSir/assignment1.js
@@ -22,8 +22,7 @@ function validators(num){
 function checkVowels(data){
     let vowels=0
     let con=0
-    let newArray=data.split("")
-    
+    let newArray=data.split(" ")      
 
     // find the longest word 
     let result=[]
@@ -46,72 +45,58 @@ function checkVowels(data){
             con++
         }
     }
-    return [`vowels:${vowels}`,`consonant:${con}`,result]
+    return [`vowels:${vowels}`,`consonant:${con}`,`New array after split:${newArray}`,result]
+}
+// Write a function that takes two arrays and returns:common elements elements only in first array elements only in second array
+function checkCommonValue(value1,value2){
+    let res=[]
+    for(let i=0;i<value1.length;i++){
+        for(let j=0;j<value2.length;j++){
+            if(value1[i]===value2[j]){
+                res.push(value1[i])
+            }
+        }
+    }
+    return res
+}
+// Create a function that accepts:an array of users {name, age} a callback and returns users filtered based on callback logic.
+
+//Level 4: Recursion + Deep Thinking
+// Write a recursive function to: flatten a deeply nested array [1, [2, [3, [4, 5]]]]
+let flatArr=[1, [2, [3, [4, 5]]]]
+console.log(flatArr.flat(Infinity))
+// Write a recursive function that checks if a string is a palindrome (ignore spaces and case).
+function checkPallindrome(data){
+    let rev=data.split('').reverse().join('')
+    if(rev===data){
+        console.log("Pallindrome")
+    }else{
+        console.log("Not pallindrome")
+    }
+}
+//  Level 5: Real-World Scenarios
+//Write a function bankAccount() that: returns an object with functions: deposit, withdraw, checkBalance balance must be private (closure)
+class BankAccount{
+    constructor(inititalBalance){
+        this.inititalBalance=inititalBalance
+    }
+    credit(amount){
+        const now=new Date()
+        let year=now.getFullYear()
+        let month=now.getMonth()+1
+        let day=now.getDay()
+        let hour=now.getHours()
+        let min=now.getMinutes()
+        let sec=now.getSeconds()
+
+        let total_amount=this.inititalBalance+amount
+        return `Amount ${amount} credit  and total amount ${total_amount} at ${year}/${month}/${day} ${hour}:${min}:${sec}`
+    }
 }
 
+let bank=new BankAccount(343434)
+console.log(bank.credit(454545))
 
-
-
-
-// Write a function that takes two arrays and returns:common elements elements only in first array elements only in second array
-
-// ⚙️ Level 2: Functions Returning Functions
-
-// Write a function calculator(operation) that returns another function.
-
-// operation can be "add", "subtract", "multiply"
-
-// Returned function should accept two numbers and perform the operation.
-
-// Create a function discountCalculator(discountPercent) that returns a function.
-
-// Returned function accepts price
-
-// Returns discounted price
-
-// 🔁 Level 3: Callback & Higher-Order Functions
-
-// Write a function processArray(arr, callback) that:
-
-// applies the callback to each element
-
-// returns a new array
-
-// Create a function that accepts:
-
-// an array of users {name, age}
-
-// a callback
-// and returns users filtered based on callback logic.
-
-// 🧠 Level 4: Recursion + Deep Thinking
-
-// Write a recursive function to:
-
-// flatten a deeply nested array
-
-// [1, [2, [3, [4, 5]]]]
-
-
-// Write a recursive function that checks if a string is a palindrome (ignore spaces and case).
-
-// Create a function that finds the sum of digits of a number using recursion.
-
-// 🧩 Level 5: Real-World Scenarios
-
-// Write a function bankAccount() that:
-
-// returns an object with functions: deposit, withdraw, checkBalance
-
-// balance must be private (closure)
-
-// Write a function that simulates login authentication:
-
-// accepts username & password
-
-// stores attempts count
-
-// locks user after 3 failed attempts
 
 
 
@@ -122,3 +107,14 @@ console.log(validators(firstData))
 // call the function checkVowels
 let secondData="Hello sudarshan khatri , Iam javascript"
 console.log(checkVowels(secondData))
+
+
+// find the common value from two array
+let val1=[1,2,3,4,5,6,7,78,434,67,34,5656,67]
+let val2=[546,3,6,8,3,6,89,1]
+console.log(checkCommonValue(val1,val2))
+
+
+//check pallindrome 
+let std="aaaa"
+checkPallindrome(std)
